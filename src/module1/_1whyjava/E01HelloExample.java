d5a1fde46327f241a0134616a7f9b178d444b74e
angelinoroibalgiannini <angelinoroibalgiannini@sidians-mbp>
2025-05-29 17:48:56 -0600

    First Push

--- src/module1/_1whyjava/E01HelloExample.java
+++ src/module1/_1whyjava/E01HelloExample.java
@@ -3,6 +3,7 @@ package module1._1whyjava;
 public class E01HelloExample {
 	//Exercise 1: After you run this code, modify it to print your name
    public static void main(String[] args) {
-      System.out.println("Hi there!");
+      System.out.println("Hi there Teo!");
    }
 }
+// my message is poop
