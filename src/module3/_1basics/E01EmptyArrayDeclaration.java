c63b218318d722f49a7406a5215ee20cb8151d4f
angelinoroibalgiannini <angelinoroibalgiannini@sidians-mbp>
2025-06-11 18:27:21 -0600

    commit

--- src/module3/_1basics/E01EmptyArrayDeclaration.java
+++ src/module3/_1basics/E01EmptyArrayDeclaration.java
@@ -7,10 +7,11 @@ public class E01EmptyArrayDeclaration {
 		// Array example
 		int[] highScores = new int[10];
 		// Add an array of 5 doubles called prices.
-
+		double[] prices = new double[5];
 		// Add an array of 5 Strings called names.
-
+		String[] names = new String[5];
 		System.out.println("Array highScores declared with size " + highScores.length);
 		// Print out the length of the new arrays
+		System.out.println("Array highScores declared with size " + names.length);
 	}
 }
